64633a8157f30604adac197d3e26bcb2a61e05b0
yanchen01 <ychen01@bu.edu>
2019-12-29 17:53:46 -0500

    added backend server and routes for user and post

---
+++ piazza2/backend/models/user.model.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        minlength: 3
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        require: true,
+        trim: true,
+        minlength: 4,
+    }
+}, {
+    timestamps: true,
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
